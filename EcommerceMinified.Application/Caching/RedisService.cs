0fb0fda8334cab18a7eb6466695714483803b248
Arthur Santana <arthurgn.santana@gmail.com>
2025-02-25 19:44:50 -0300

    refactor: update RedisService methods to use Guid as key for improved data retrieval

--- EcommerceMinified.Application/Caching/RedisService.cs
+++ EcommerceMinified.Application/Caching/RedisService.cs
@@ -7,14 +7,16 @@ namespace EcommerceMinified.Application.Caching;
 
 public class RedisService(IDistributedCache _distributedCache) : IRedisService
 {
-    public async Task<T?> GetAsync<T>(string key)
+    public async Task<T?> GetAsync<T>(Guid id)
     {
+        var key = $"{nameof(T)}_{id}";
         var value = await _distributedCache.GetStringAsync(key);
         return value is null ? default : JsonSerializer.Deserialize<T>(value);
     }
 
-    public void SetAsync<T>(string key, T value, TimeSpan? expiration = null)
+    public void SetAsync<T>(Guid id, T value, TimeSpan? expiration = null)
     {
+        var key = $"{nameof(T)}_{id}";
         var options = new DistributedCacheEntryOptions();
 
         if (expiration.HasValue)
