a4286ac42b0747f0d371df56f487a71230e1d034
Arthur Santana <arthurgn.santana@gmail.com>
2025-02-26 18:14:05 -0300

    feat: add resilience pipeline configuration with retry and circuit breaker strategies

--- EcommerceMinified.IoC/DependencyContainer.cs
+++ EcommerceMinified.IoC/DependencyContainer.cs
@@ -13,6 +13,9 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
 
 namespace EcommerceMinified.IoC;
 
@@ -64,6 +67,30 @@ public class DependencyContainer
             options.InstanceName = "EcommerceMinified";
         });
         services.AddScoped<IRedisService, RedisService>();
+        #endregion
+
+        #region ResiliencePipeline
+        services.AddResiliencePipeline("default", builder =>
+        {
+            builder.AddRetry(new RetryStrategyOptions
+            {
+                Delay = TimeSpan.FromSeconds(2), // Tempo de espera inicial entre as tentativas de repetição
+                MaxRetryAttempts = 2, // Número máximo de tentativas de repetição
+                BackoffType = DelayBackoffType.Exponential, // Tipo de backoff (exponencial)
+                UseJitter = true // Adiciona variação aleatória ao tempo de espera entre as tentativas de repetição
+            });
+
+            builder.AddCircuitBreaker(new CircuitBreakerStrategyOptions
+            {
+                FailureRatio = 0.5, // Se 50% das tentativas falharem, o circuito será aberto
+                SamplingDuration = TimeSpan.FromSeconds(10), // As falhas são monitoradas durante um período de 10 segundos
+                MinimumThroughput = 8, // Pelo menos 8 tentativas devem ocorrer durante o período de amostragem
+                BreakDuration = TimeSpan.FromSeconds(30), // O circuito permanecerá aberto por 30 segundos antes de permitir novas tentativas
+            });
+
+            builder.AddTimeout(TimeSpan.FromSeconds(5)); // Adiciona um tempo limite de 5 segundos para as operações
+        }
+        );
         #endregion
     }
 }
